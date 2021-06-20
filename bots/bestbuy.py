d20f552d5c8cf9df1a7caf11ab2b2de444ae10c4
Kyle Supple <38538913+kysu1313@users.noreply.github.com>
2021-06-20 19:13:34 -0400

    Added Requirements.txt

--- bots/bestbuy.py
+++ bots/bestbuy.py
@@ -1,4 +1,4 @@
-
+from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import NoSuchElementException
@@ -9,9 +9,6 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 import time
-
-from selenium import webdriver
-
 import random
 import re
 from colorama import Fore, Style, init
@@ -28,7 +25,7 @@ from datetime import date, datetime
 from time import sleep
 from tqdm import tqdm
 import logging
-from dotenv import load_dotenv
+#from dotenv import load_dotenv
 from bots.browser import Browser 
 from bots.printer import Printer 
 from bots.purchase import Purchase
@@ -57,34 +54,11 @@ DEFAULT_HEADERS = {
 }
 
 
-BEST_BUY_PDP_URL = "https://api.bestbuy.com/click/5592e2b895800000/{sku}/pdp"
-BEST_BUY_CART_URL = "https://api.bestbuy.com/click/5592e2b895800000/{sku}/cart"
-
-BEST_BUY_ADD_TO_CART_API_URL = "https://www.bestbuy.com/cart/api/v1/addToCart"
-BEST_BUY_CHECKOUT_URL = "https://www.bestbuy.com/checkout/c/orders/{order_id}/"
-
-DEFAULT_HEADERS = {
-    "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-    "accept-encoding": "gzip, deflate, br",
-    "accept-language": "en-US,en;q=0.9,zh-CN;q=0.8,zh;q=0.7",
-    "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36",
-    "origin": "https://www.bestbuy.com",
-}
-
-options = Options()
-options.page_load_strategy = "eager"
-options.add_experimental_option("excludeSwitches", ["enable-automation"])
-options.add_experimental_option("useAutomationExtension", False)
-prefs = {"profile.managed_default_content_settings.images": 2}
-options.add_experimental_option("prefs", prefs)
-options.add_argument("user-data-dir=.profile-bb")
-options.add_argument("user-data-dir=/home/kms/.config/google-chrome/Defaule")
-
 class BestBuy():
 
     def __init__(self, number, url, settings, debug=False):
 
-        load_dotenv()
+        #load_dotenv()
         init(convert=True)
 
         fhandler = logging.FileHandler(filename='.\\bestBuyLog.log', mode='a')
@@ -156,7 +130,6 @@ class BestBuy():
                 pass
             self.driver.get(self.url)
 
-
     def get_chunks(self, desc):
         new_desc = desc.split("\n")[0]
         return new_desc
@@ -187,7 +160,6 @@ class BestBuy():
         try:
             if self.driver.find_element_by_id("survey_invite_no"):
                 self.driver.find_element_by_id("survey_invite_no").click()
-
         except NoSuchElementException:
             print("No popup")
 
@@ -197,7 +169,6 @@ class BestBuy():
             if self.driver.find_element_by_xpath("//*[@id=\"widgets-view-email-modal-mount\"]/div/div/div[1]/div/div/div/div/button"):
                 self.driver.find_element_by_xpath(
                     "//*[@id=\"widgets-view-email-modal-mount\"]/div/div/div[1]/div/div/div/div/button").click()
-
         except NoSuchElementException:
             print("no popup")
 
@@ -209,7 +180,6 @@ class BestBuy():
         if not is_in or "Sold Out" in description.split("\n"):
             in_stock = False
             self.printer.output(ctype, "OUT", "xxx", description, "", self.stream_mode, "BestBuy")
-
         else:
             in_stock = True
             print_desc = description[:20]
